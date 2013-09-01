a29d7209d92c4ef9b584096206d2fa35b5d67562
adrian.nitu <adrian.nitu.92@gmail.com>
2013-09-01 19:21:21 +0300

    Solved the examples, added coding style

--- ReverseMap.hs
+++ ReverseMap.hs
@@ -1,6 +1,18 @@
 
-functs = (3 *) : ((2 +) : [])
+reverseMap :: a -> [a -> b] -> [b]
+--reverseMap takes a parameter a, a list of functions and returns a list of b's
+--the functions type are : takes parameter a and returns parameter b
+reverseMap value = map (\x -> x value)
 
-value = 5
+--Exemplu de input: 4 si [add_one, multiply_by_five] 
+--		=> output-ul va fi [5, 20]. 
+exemplul1 = [(1+),(5*)]
 
-solve = map (\x -> x value) functs
+--Alt exemplu de input: 7 si [subtract_three, add_two,
+--		 multiply_by_three] => output-ul va fi [4, 9, 21].
+exemplul2 = [(subtract 3), --( + (-3)) 
+					(+2), (3*)]
+
+solveEx1 = reverseMap 4 exemplul1
+solveEx2 = reverseMap 7 exemplul2
+
