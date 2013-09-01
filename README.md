769ac8869072b1b79acbe8eed6a7fddb04fdb2cd
adrian.nitu <adrian.nitu.92@gmail.com>
2013-09-01 19:33:38 +0300

    Removed some inconsistencies in my coding style

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ReverseMap
 ==========
+Instead of aplying a function on a list of values, we apply all functions in a list on a value.
--- ReverseMap.hs
+++ ReverseMap.hs
@@ -10,9 +10,10 @@ exemplul1 = [(1+),(5*)]
 
 --Alt exemplu de input: 7 si [subtract_three, add_two,
 --		 multiply_by_three] => output-ul va fi [4, 9, 21].
-exemplul2 = [(subtract 3), --( + (-3)) 
-					(+2), (3*)]
+exemplul2 = [(subtract 3), --((-3) +) 
+					(2+), (3*)]
 
 solveEx1 = reverseMap 4 exemplul1
 solveEx2 = reverseMap 7 exemplul2
 
+ano value = (\x -> x value)
